36be23b6fa05593edd851f5e1c4dad1d40fba2a5
Ubuntu <ubuntu@ip-172-31-25-58.us-west-2.compute.internal>
2018-11-12 02:08:18 +0000

    add a script for unzipping datasets young2old

---
+++ scripts/unzip_datasets.sh
@@ -0,0 +1,5 @@
+tar zxvf datasets/young2old/trainA.tar.gz -C datasets/young2old/
+tar zxvf datasets/young2old/trainB.tar.gz -C datasets/young2old/
+tar zxvf datasets/young2old/testA.tar.gz -C datasets/young2old/
+tar zxvf datasets/young2old/testB.tar.gz -C datasets/young2old/
+rm datasets/young2old/*.tar.gz
